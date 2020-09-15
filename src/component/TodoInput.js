4183c305b5f3ca659cf09fa7633837c65a5458c1
김태연 <ty980407@gmail.com>
2020-09-15 19:16:20 +0900

    Styled-Component 적용

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './TodoInput.css';
+// import './TodoInput.css';
+import styled from "styled-components";
 
 // const TodoInput = ({ text, onClick, onChange })
 
@@ -7,20 +8,40 @@ class TodoInput extends Component {
     render() {
         const { inputText, onChange, onClick, onKeyPress } = this.props;
         return(
-            <div className="input-form">
-                <input
+            <TodoInputDiv>
+                <InputText
                     value={ inputText }
                     type="text"
                     className="input-text"
                     placeholder="오늘 할 일을 입력하세요."
                     onChange={ onChange }
+                    // onKeyPress={ (e) => onKeyPress(e) }>
                     onKeyPress={ onKeyPress }>
-                </input>
-                <span 
+                </InputText>
+                <InputBtn 
                     className="input-btn"
-                    onClick={ onClick }><i class="fas fa-check"></i></span>
-            </div>
+                    onClick={ onClick }><i className="fas fa-check"></i></InputBtn>
+            </TodoInputDiv>
         )
     }
 }
+
+const TodoInputDiv = styled.div`
+    padding: 20px;
+    border-bottom: 1px solid grey;
+`
+
+const InputText = styled.input`
+    background: 0%;
+    border: none;
+    width: 90%;
+    font-size: 15px;
+    outline: none;
+`
+
+const InputBtn = styled.span`
+    float: right;
+    cursor: pointer;
+`
+
 export default TodoInput;
